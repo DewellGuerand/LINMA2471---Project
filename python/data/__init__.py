6450babc25417e1406728960d13c4f492bcdec6b
Ahou Lucas <ahou.lucas@gmail.com>
2025-12-08 14:57:54 +0100

    Added train_test split in data processor

--- python/data/__init__.py
+++ python/data/__init__.py
@@ -5,10 +5,12 @@ from .data_processor import (
     DataProcessor,
     load_data,
     get_initial_portfolio,
+    evaluate_portfolio,
 )
 
 __all__ = [
     "DataProcessor",
     "load_data",
     "get_initial_portfolio",
+    "evaluate_portfolio",
 ]
